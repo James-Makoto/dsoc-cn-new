d01f56c3ad6d32be27cfdd995917891ad88f77cb
imahane <451835357@qq.com>
2024-09-01 17:20:18 +0800

    补一点缺

--- text/to_json.py
+++ text/to_json.py
@@ -3,4 +3,16 @@ from trans import Translation
 
 for name in ('dsoc_event',):
     trans = Translation(f'{name}.xlsx')
-    trans.save(f'{name}.json', index='Jpn')
+    trans.save(f'{name}.json', index='Tag')
+
+for name in ('bible',):
+    trans = Translation(f'{name}.xlsx')
+    trans.save(f'{name}.json', index='Tag')
+
+for name in ('fusion',):
+    trans = Translation(f'{name}.xlsx')
+    trans.save(f'{name}.json', index='Tag')
+
+for name in ('code.bin',):
+    trans = Translation(f'{name}.xlsx')
+    trans.save(f'{name}.json', index='Tag')
