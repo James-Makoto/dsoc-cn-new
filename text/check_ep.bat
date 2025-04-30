4b961a7c45695cbc0030f283aaa3ede5019819c8
James-Makoto <55654427+James-Makoto@users.noreply.github.com>
2025-04-30 10:09:13 +0800

    20250429 forget (#78)
    * Update code.bin.json
    
    * Update dsoc_event.json
    
    * Update dsoc_event.json
    
    * Update dsoc_event.json
    
    * Update dsoc_event.json
    
    * Update dsoc_event.json
    
    * Update code.bin.json
    
    * Update code.bin.json
    
    * Update dsoc_event.json
    
    * Update code.bin.json
    
    * update .bat for convenience

---
+++ text/check_ep.bat
@@ -0,0 +1,6 @@
+python check_ep.py bible.json
+python check_ep.py code.bin.json
+python check_ep.py ds_auction.json
+python check_ep.py dsoc_event.json
+python check_ep.py fusion.json
+pause
---
+++ text/check_halwidth.bat
@@ -0,0 +1,6 @@
+python check_halfwidth.py bible.json
+python check_halfwidth.py code.bin.json
+python check_halfwidth.py ds_auction.json
+python check_halfwidth.py dsoc_event.json
+python check_halfwidth.py fusion.json
+pause
