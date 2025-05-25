349c975dc9a434799acb3d2494f37314f13e4fcd
James-Makoto <55654427+James-Makoto@users.noreply.github.com>
2025-05-26 01:49:31 +0800

    1

---
+++ text/check_length.bat
@@ -0,0 +1,6 @@
+python check_length.py -f bible.json
+python check_length.py -f code.bin.json
+python check_length.py -f ds_auction.json
+python check_length.py -f dsoc_event.json
+python check_length.py -f fusion.json
+pause
